1dc1770b3141a96ab1d3793c827d461775f31315
root <root@ip-172-31-35-199.eu-north-1.compute.internal>
2023-10-02 13:05:38 +0000

    naidu file commmit

---
+++ naidu/src/main/java/nishvika/App.java
@@ -0,0 +1,13 @@
+package nishvika;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
